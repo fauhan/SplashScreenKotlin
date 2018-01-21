bf55f65c7a89cc407f8ec332a8ce3265117b8939
fauhan.pugar <fauhan.pugar@emporiadigital.com>
2018-01-21 19:43:18 +0700

    splash screen

---
+++ app/src/main/java/com/inpows/splashscreenkotlin/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.inpows.splashscreenkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by fauhan.pugar on 1/21/2018.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
